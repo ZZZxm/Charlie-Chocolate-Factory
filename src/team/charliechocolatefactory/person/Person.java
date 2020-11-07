b68b8829995a7b49d980682226823025803cb149
Brian Chiu <2440720776@qq.com>
2020-11-07 12:49:02 +0800

    Test for creating class

---
+++ src/team/charliechocolatefactory/person/Person.java
@@ -0,0 +1,12 @@
+package team.charliechocolatefactory.person;
+
+/**
+ * @author Brian.Z
+ * @project chocolateFactory
+ * @classname Person
+ * @description TODO
+ * @date 2020/11/7 12:48
+ */
+public class Person {
+    
+}
